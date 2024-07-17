306a6b161520c0308c9a1ea24896e139813bf8d4
Davasz <davidavi200615@gmail.com>
2024-07-17 18:59:26 -0300

    finish calculator

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,62 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Iniciando calculadora...");
+        System.out.println(" 1 - Adição \n 2 - Subtração \n 3 - Multiplicação \n 4 - Divisão \n 5 - Raiz Quadrada \n 6 - Exponenciação");
+        System.out.println("Seleciona a operação que você deseja: ");
+
+        int operation = input.nextInt();
+
+        if(operation < 1 || operation > 6) {
+            System.out.println("Operação inválida. Tente novamente.");
+            return;
+        }
+
+        System.out.printf("Operação selecionada: %d", operation);
+
+        System.out.println("\nDigite o primeiro número: ");
+        double num1 = input.nextDouble();
+        double num2 = 0;
+
+        if(operation != 5) {
+            System.out.println("Digite o segundo número: ");
+            num2 = input.nextDouble();
+        }
+
+        double result = 0;
+
+        switch (operation) {
+            case 1:
+                result = num1 + num2;
+                break;
+            case 2:
+                result = num1 - num2;
+                break;
+            case 3:
+                result = num1 * num2;
+                break;
+            case 4:
+                if(num2 == 0) {
+                    System.out.println("Não é possível dividir por zero.");
+                    return;
+                }
+                result = num1 / num2;
+                break;
+            case 5:
+                if(num1 < 0) {
+                    System.out.println("A raiz quadrada de um número negativo não existe.");
+                    return;
+                }
+                result = Math.sqrt(num1);
+                break;
+            case 6:
+                result = Math.pow(num1, num2);
+                break;
+        }
+
+        System.out.printf("Resultado: %.2f%n", result);
     }
 }
